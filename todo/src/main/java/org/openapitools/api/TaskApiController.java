e5a186c647603477932e5719fef58195a9e458cf
Rafał Mecwaldowski <93916670+rmecwaldowski-pgs@users.noreply.github.com>
2023-04-02 21:01:40 +0200

    Initial commit

---
+++ todo/src/main/java/org/openapitools/api/TaskApiController.java
@@ -0,0 +1,49 @@
+package org.openapitools.api;
+
+import org.openapitools.model.TaskCreateRequest;
+import org.openapitools.model.TaskResponse;
+import org.openapitools.model.TaskUpdateRequest;
+import java.util.UUID;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.context.request.NativeWebRequest;
+
+import javax.validation.constraints.*;
+import javax.validation.Valid;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import javax.annotation.Generated;
+
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2023-04-02T20:47:24.972943100+02:00[Europe/Warsaw]")
+@Controller
+@RequestMapping("${openapi.todo.base-path:}")
+public class TaskApiController implements TaskApi {
+
+    private final NativeWebRequest request;
+
+    @Autowired
+    public TaskApiController(NativeWebRequest request) {
+        this.request = request;
+    }
+
+    @Override
+    public Optional<NativeWebRequest> getRequest() {
+        return Optional.ofNullable(request);
+    }
+
+}
