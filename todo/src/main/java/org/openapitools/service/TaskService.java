76b63b62da19308dd0bbc54c55f7128829b65c7a
Rafał Mecwaldowski <93916670+rmecwaldowski-pgs@users.noreply.github.com>
2023-05-31 21:24:20 +0200

    Calendar events in progress

--- todo/src/main/java/org/openapitools/service/TaskService.java
+++ todo/src/main/java/org/openapitools/service/TaskService.java
@@ -1,9 +1,15 @@
 package org.openapitools.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.aspectj.apache.bcel.classfile.annotation.NameValuePair;
 import org.openapitools.api.ApiException;
 import org.openapitools.model.dto.CreateReminderRequest;
 import org.openapitools.model.dto.TaskCreateRequest;
@@ -12,6 +18,7 @@ import org.openapitools.model.dto.TaskUpdateRequest;
 import org.openapitools.model.entity.Task;
 import org.openapitools.model.mapper.TaskMapper;
 import org.openapitools.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
@@ -19,10 +26,15 @@ import org.springframework.stereotype.Service;
 public class TaskService {
 	private final TaskRepository taskRepository;
 	private final TaskMapper taskMapper;
+	private String eventUrl;
+	private String apiKey;
 
-	public TaskService(TaskRepository taskRepository, TaskMapper taskMapper) {
+	public TaskService(TaskRepository taskRepository, TaskMapper taskMapper,
+			@Value("${googleEventsUrl}") String eventUrl, @Value("${googleApiKey}") String apiKey ) {
 		this.taskRepository = taskRepository;
 		this.taskMapper = taskMapper;
+		this.eventUrl = eventUrl;
+		this.apiKey = apiKey;
 	}
 
 	public List<TaskResponse> getAllTasksByUserId(String userId) {
@@ -75,7 +87,19 @@ public class TaskService {
 	public TaskResponse createTaskReminder(UUID taskId, CreateReminderRequest reminderRequest) throws ApiException {
 		Optional<Task> existingTask = taskRepository.findTaskById(taskId);
 		if (existingTask.isPresent()) {
-			//And user type GOOGLE
+			//When user type GOOGLE
+			HttpPost httppost = new HttpPost(eventUrl);
+
+			List<NameValuePair> params = new ArrayList<>(1);
+			params.add(new BasicNameValuePair("key", apiKey));
+
+			httppost.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));
+
+			httppost.addHeader("Authorization", "Bearer " + accessToken);
+			httppost.addHeader("Accept", "application/json");
+			httppost.addHeader("Content-Type", "application/json");
+
+			String jsonString = EntityUtils.toString(httpclient.execute(httppost).getEntity());
 			return new TaskResponse(UUID.randomUUID(), "test");
 		} else {
 			throw new ApiException(HttpStatus.NOT_FOUND.value());
