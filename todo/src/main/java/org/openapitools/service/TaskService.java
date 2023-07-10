ccfa32e95ad783869c5e7f1f53d54d22e5c195a8
Rafał Mecwaldowski <93916670+rmecwaldowski-pgs@users.noreply.github.com>
2023-07-10 21:43:37 +0200

    Added connection with Auth0

--- todo/src/main/java/org/openapitools/service/TaskService.java
+++ todo/src/main/java/org/openapitools/service/TaskService.java
@@ -1,15 +1,14 @@
 package org.openapitools.service;
 
-import java.io.IOException;
-import java.security.GeneralSecurityException;
 import lombok.RequiredArgsConstructor;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
 import org.openapitools.api.ApiException;
-import org.openapitools.model.dto.GlobalUser;
 import org.openapitools.model.dto.CreateReminderRequest;
 import org.openapitools.model.dto.TaskCreateRequest;
 import org.openapitools.model.dto.TaskResponse;
@@ -18,7 +17,7 @@ import org.openapitools.model.entity.Task;
 import org.openapitools.model.mapper.TaskMapper;
 import org.openapitools.repository.TaskRepository;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -28,56 +27,75 @@ public class TaskService {
 	private final TaskMapper taskMapper;
 	private final GoogleCalendarApiService googleCalendarApiService;
 
-	public List<TaskResponse> getAllTasksByUserId() {
-		GlobalUser globalUser = (GlobalUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-		List<Task> taskList = taskRepository.findAllTaskByUserId(globalUser.getId());
-		return taskMapper.listTaskToListTaskResponse(taskList);
-	}
-
-	public TaskResponse getTaskByUserIdAndTaskId(Long taskId) throws ApiException {
-		GlobalUser globalUser = (GlobalUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-		Task task = taskRepository.findTaskByUserIdAndId(globalUser.getId(), taskId).orElseThrow(() -> new ApiException(HttpStatus.NOT_FOUND.value()));
-		return taskMapper.taskToTaskResponse(task);
-	}
-
-	public TaskResponse createTask(TaskCreateRequest taskCreateRequest) throws ApiException {
-		GlobalUser globalUser = (GlobalUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-		Optional<Task> existingTask = taskRepository.findTaskByNameAndUserId(taskCreateRequest.getName(), globalUser.getId());
-		if (existingTask.isEmpty()) {
-			Task task = new Task();
-			task.setName(taskCreateRequest.getName());
-			task.setUserId(globalUser.getId());
-			task.setDueDate(taskCreateRequest.getDueDate());
-			task.setCompleted(false);
-			taskRepository.saveAndFlush(task);
-			return taskMapper.taskToTaskResponse(task);
+	public List<TaskResponse> getAllTasksByUserId(OidcUser oidcUser) {
+		Long id = oidcUser.getAttribute("sub");
+		if (id != null) {
+			List<Task> taskList = taskRepository.findAllTaskByUserId(id);
+			return taskMapper.listTaskToListTaskResponse(taskList);
 		} else {
-			throw new ApiException(HttpStatus.CONFLICT.value());
+			return new ArrayList<>(10);
 		}
 	}
 
-	public TaskResponse updateTask(Long taskId, TaskUpdateRequest taskUpdateRequest) throws ApiException {
-		GlobalUser globalUser = (GlobalUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-		Optional<Task> existingTask = taskRepository.findTaskByUserIdAndId(globalUser.getId(), taskId);
-		if (existingTask.isPresent()) {
-			existingTask.get().setName(taskUpdateRequest.getName() != null ? taskUpdateRequest.getName() : existingTask.get().getName());
-			existingTask.get().setDueDate(taskUpdateRequest.getDueDate() != null ? taskUpdateRequest.getDueDate() : existingTask.get().getDueDate());
-			existingTask.get().setCompleted(taskUpdateRequest.getIsCompleted() != null ? taskUpdateRequest.getIsCompleted() : existingTask.get().isCompleted());
-			taskRepository.saveAndFlush(existingTask.get());
-			return taskMapper.taskToTaskResponse(existingTask.get());
-		} else {
-			throw new ApiException(HttpStatus.NOT_FOUND.value());
+	public TaskResponse getTaskByUserIdAndTaskId(Long taskId, OidcUser oidcUser) throws ApiException {
+		Long id = oidcUser.getAttribute("sub");
+		if (id != null) {
+			Optional<Task> task = taskRepository.findTaskByUserIdAndId(id, taskId);
+			if (task.isPresent()) {
+				return taskMapper.taskToTaskResponse(task.get());
+			}
 		}
+		throw new ApiException(HttpStatus.NOT_FOUND.value());
 	}
 
-	public void deleteTask(Long taskId) throws ApiException {
-		GlobalUser globalUser = (GlobalUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-		Optional<Task> existingTask = taskRepository.findTaskByUserIdAndId(globalUser.getId(), taskId);
-		if (existingTask.isPresent()) {
-			taskRepository.delete(existingTask.get());
-		} else {
-			throw new ApiException(HttpStatus.NOT_FOUND.value());
+	public TaskResponse createTask(TaskCreateRequest taskCreateRequest, OidcUser oidcUser) throws ApiException {
+		Long id = oidcUser.getAttribute("sub");
+		if (id != null) {
+			Optional<Task> existingTask = taskRepository.findTaskByNameAndUserId(taskCreateRequest.getName(), id);
+			if (existingTask.isEmpty()) {
+				Task task = new Task();
+				task.setName(taskCreateRequest.getName());
+				task.setUserId(id);
+				task.setDueDate(taskCreateRequest.getDueDate());
+				task.setCompleted(false);
+				taskRepository.saveAndFlush(task);
+				return taskMapper.taskToTaskResponse(task);
+			} else {
+				throw new ApiException(HttpStatus.CONFLICT.value());
+			}
 		}
+		throw new ApiException(HttpStatus.FORBIDDEN.value());
+	}
+
+	public TaskResponse updateTask(Long taskId, TaskUpdateRequest taskUpdateRequest, OidcUser oidcUser) throws ApiException {
+		Long id = oidcUser.getAttribute("sub");
+		if (id != null) {
+			Optional<Task> existingTask = taskRepository.findTaskByUserIdAndId(id, taskId);
+			if (existingTask.isPresent()) {
+				existingTask.get().setName(taskUpdateRequest.getName() != null ? taskUpdateRequest.getName() : existingTask.get().getName());
+				existingTask.get().setDueDate(taskUpdateRequest.getDueDate() != null ? taskUpdateRequest.getDueDate() : existingTask.get().getDueDate());
+				existingTask.get()
+						.setCompleted(taskUpdateRequest.getIsCompleted() != null ? taskUpdateRequest.getIsCompleted() : existingTask.get().isCompleted());
+				taskRepository.saveAndFlush(existingTask.get());
+				return taskMapper.taskToTaskResponse(existingTask.get());
+			} else {
+				throw new ApiException(HttpStatus.NOT_FOUND.value());
+			}
+		}
+		throw new ApiException(HttpStatus.CONFLICT.value());
+	}
+
+	public void deleteTask(Long taskId, OidcUser oidcUser) throws ApiException {
+		Long id = oidcUser.getAttribute("sub");
+		if (id != null) {
+			Optional<Task> existingTask = taskRepository.findTaskByUserIdAndId(id, taskId);
+			if (existingTask.isPresent()) {
+				taskRepository.delete(existingTask.get());
+			} else {
+				throw new ApiException(HttpStatus.NOT_FOUND.value());
+			}
+		}
+		throw new ApiException(HttpStatus.CONFLICT.value());
 	}
 
 	public TaskResponse createTaskReminder(Long taskId, CreateReminderRequest reminderRequest) throws ApiException {
